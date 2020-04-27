86983c0cbc1ad219c621f8e1f739979fc3db92f1
Robert Metcalf <rmetcalf9@googlemail.com>
2020-04-27 17:56:50 +0100

    Added code to deal with disconnects

--- mq_client_abstraction/stomp_client/connection.py
+++ mq_client_abstraction/stomp_client/connection.py
@@ -9,39 +9,60 @@ class ConnectionClass():
   closed = None
   recieveFunction = None
   registeredSubscriptions = None
+  connected = None
 
   def __init__(self, fullConnectionDetails, recieveFunction):
     self.closed = False
     self.fullConnectionDetails = fullConnectionDetails
     self.recieveFunction = recieveFunction
     self.registeredSubscriptions = []
+    self.connected = False
 
     self.stompConnection = stomp.Connection(
-      host_and_ports=[(fullConnectionDetails["FormattedConnectionDetails"]["Url"], fullConnectionDetails["FormattedConnectionDetails"]["Port"])])
+      host_and_ports=[(self.fullConnectionDetails["FormattedConnectionDetails"]["Url"], self.fullConnectionDetails["FormattedConnectionDetails"]["Port"])])
 
-    if fullConnectionDetails["FormattedConnectionDetails"]["Protocol"] == "stomp":
+    self._connectIfNeeded()
+
+  def _connectIfNeeded(self):
+    if self.connected:
+      return
+    if self.fullConnectionDetails["FormattedConnectionDetails"]["Protocol"] == "stomp":
       pass
-    elif fullConnectionDetails["FormattedConnectionDetails"]["Protocol"] == "stomp+ssl":
-        self.stompConnection.set_ssl(
-        for_hosts=[(fullConnectionDetails["FormattedConnectionDetails"]["Url"], fullConnectionDetails["FormattedConnectionDetails"]["Port"])],
+    elif self.fullConnectionDetails["FormattedConnectionDetails"]["Protocol"] == "stomp+ssl":
+      self.stompConnection.set_ssl(
+        for_hosts=[(self.fullConnectionDetails["FormattedConnectionDetails"]["Url"],
+                    self.fullConnectionDetails["FormattedConnectionDetails"]["Port"])],
         ssl_version=ssl.PROTOCOL_TLSv1_2)
     else:
       raise Exception("Unknown protocol")
 
     self.stompConnection.connect(
-      fullConnectionDetails["Username"],
-      fullConnectionDetails["Password"],
+      self.fullConnectionDetails["Username"],
+      self.fullConnectionDetails["Password"],
       wait=True
     )
+    self.connected = True
+
+  def _onDisconnected(self):
+    self.connected = False
+    # If we have subscribers then we must reconnect and register all the subscriptions again
+    #  otherwise we cna wait and only reconnect when sendStringMessage or registerSubscription is called
+    if len(self.registeredSubscriptions) == 0:
+      return
+    self._connectIfNeeded()
+    for internalDestination in self.registeredSubscriptions():
+      self.stompConnection.subscribe(destination=internalDestination, id=1, ack='auto')
 
   def sendStringMessage(self, internalDestination, body):
     if self.closed:
       raise MqClientExceptionClass("Trying to send message on closed STOMP connection")
+    self._connectIfNeeded()
     self.stompConnection.sendMessage(body=body, destination=internalDestination)
 
   def registerSubscription(self, internalDestination):
+    self._connectIfNeeded()
     if len(self.registeredSubscriptions) == 0:
-      self.stompConnection.set_listener('', StompConnectionListenerClass(self.recieveFunction))
+      self.stompConnection.set_listener('', StompConnectionListenerClass(recieveFunction=self.recieveFunction, disconnectedFunciton=self._onDisconnected))
     self.registeredSubscriptions.append(internalDestination)
     self.stompConnection.subscribe(destination=internalDestination, id=1, ack='auto')
 
--- mq_client_abstraction/stomp_client/stompConnectionListener.py
+++ mq_client_abstraction/stomp_client/stompConnectionListener.py
@@ -2,11 +2,15 @@ import stomp
 
 class StompConnectionListenerClass(stomp.ConnectionListener):
   recieveFunction = None
+  disconnectedFunciton = None
 
-  def __init__(self, recieveFunction):
+  def __init__(self, recieveFunction, disconnectedFunciton):
     self.recieveFunction = recieveFunction
+    self.onDisconnectedFunciton = disconnectedFunciton
 
   def on_error(self, headers, message):
     raise Exception("Error", message)
   def on_message(self, headers, message):
     self.recieveFunction(internalDestination=headers["destination"], body=message)
+  def on_disconnected(self):
+      self.disconnectedFunciton()
