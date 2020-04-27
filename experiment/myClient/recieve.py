3f7adbb0cf533adf4cca6f677c6adafd1be9a545
Robert Metcalf <rmetcalf9@googlemail.com>
2020-04-27 19:02:24 +0100

    Initial working version tested

--- experiment/myClient/recieve.py
+++ experiment/myClient/recieve.py
@@ -4,14 +4,28 @@ import time
 
 mqClient = Common.getMqClient()
 
-recieveQueue = queue.Queue()
+destinationToTest="/queue/test"
 
-mqClient.subscribeDestinationToPythonQueue(destination="/queue/test", queue=recieveQueue)
-mqClient.startRecieveThread(sleepTime=0.1)
-time.sleep(0.3)
-mqClient.close(wait=True)
-while not recieveQueue.empty():
-  (message) = recieveQueue.get()
-  print("Recieved ", message)
+#recieveQueue = queue.Queue()
+#mqClient.subscribeDestinationToPythonQueue(destination=destination, queue=recieveQueue)
+# mqClient.startRecieveThread(sleepTime=0.1)
+# time.sleep(0.3)
+# mqClient.close(wait=True)
+# while not recieveQueue.empty():
+#   (message) = recieveQueue.get()
+#   print("Recieved ", message)
+
+def msgRecieveFunction(destination, body):
+  print("Recieved ", body, " sent to " , destination)
+
+mqClient.subscribeToDestination(destination=destinationToTest, msgRecieveFunction=msgRecieveFunction)
+
+try:
+    while True:
+        time.sleep(10)
+except KeyboardInterrupt:
+    print('interrupted - so exiting!')
+
+
 
 # python3 ./recieve.py
